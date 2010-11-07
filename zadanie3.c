0fa2d6011a65833a7010d3ed16ae620ae771d550
JacekG <jacek.galka@neosteel.pl>
2010-11-07 12:48:18 +0100

    zadanie3 poprawne

---
+++ zadanie3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+
+int tabela[] = {1, 2, 4, 6, 12};
+ int i=4;
+
+
+ while (i >= 0)
+ {
+   printf("%d  ", tabela[i]);
+   --i;
+ }
+   printf("\n");
+
+
+   return 0;
+}
