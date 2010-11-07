72f1517641b4800f24a10521dfef6fa89aeb8d17
JacekG <jacek.galka@neosteel.pl>
2010-11-07 10:28:31 +0100

    gotowe zadanie1

---
+++ zadanie1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n1, n2, n3
+
+    n1 = 5+3*8/2-3
+    n2 = 2%2+2*2-2/2
+    n3 = 2*4*(5+9/2)
+
+    printf("%d\n", n1);
+  printf("%d\n", n2);
+  printf("%d\n", n3);
+
+  return 0;
+
+}
